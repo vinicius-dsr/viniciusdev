49be8c3fa7a352374f6ab6fbd08c88a5ffee06b5
Vinícius dos Santos Reis <viniciusreis3105@gmail.com>
2026-08-03 19:46:42 -0300

    feat: add footer with social links

---
+++ src/data/socialLinks.ts
@@ -0,0 +1,22 @@
+export const socialLinks = [
+  {
+    name: "GitHub",
+    icon: "github",
+    href: "https://github.com/vinicius-dsr",
+  },
+  {
+    name: "LinkedIn",
+    icon: "linkedin",
+    href: "https://www.linkedin.com/in/viniciusreis-pgm/",
+  },
+  {
+    name: "Instagram",
+    icon: "instagram",
+    href: "https://www.instagram.com/vinicius_dev/",
+  },
+  {
+    name: "WhatsApp",
+    icon: "whatsapp",
+    href: "https://wa.me/5591992761377?text=Olá,%20vim%20pelo%20site",
+  },
+];
