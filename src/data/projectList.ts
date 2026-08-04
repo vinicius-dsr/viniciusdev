cc2754a9783c90fc6b73cc0d8f51559ba86fc8f1
Vinícius dos Santos Reis <viniciusreis3105@gmail.com>
2026-08-03 23:59:46 -0300

    feat: add external file with datas about project and service for dinamic manager

---
+++ src/data/projectList.ts
@@ -0,0 +1,14 @@
+export const projects = [
+  { name: "Vorthal", stack: "Full Stack", imageUrl: "/vorthal.png" },
+  { name: "Froz Advogados", stack: "Full Stack", imageUrl: "/froz.png" },
+  {
+    name: "Mise en Place",
+    stack: "Full Stack",
+    imageUrl: "/mise-en-place.avif",
+  },
+  {
+    name: "Quilombo Santa Rosa",
+    stack: "Full Stack",
+    imageUrl: "/quilombo.avif",
+  },
+];
